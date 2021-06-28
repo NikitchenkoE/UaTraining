443d967cae177f4f20f2a66754b05ee9e70f76ea
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-06-28 13:03:17 +0300

    Add files via upload

---
+++ tasks2/GameTask/src/View.java
@@ -0,0 +1,7 @@
+public class View {
+    public final String BROKEN_APP = "Only numeric values are allowed";
+
+    public void massageReader(String message){
+        System.out.println(message);
+    }
+}
