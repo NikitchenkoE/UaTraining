9f7d9f77c45ade957eeaee16c79922b8820fa616
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-06-29 19:16:39 +0300

    Delete tasks2 directory

--- tasks2/GameTask/src/View.java
+++
@@ -1,7 +0,0 @@
-public class View {
-    public final String BROKEN_APP = "Only numeric values are allowed";
-
-    public void massageReader(String message){
-        System.out.println(message);
-    }
-}
