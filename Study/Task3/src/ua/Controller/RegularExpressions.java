3e8e1886cd08f83febb9b814abae0c63fdeb32a0
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-07-05 21:11:02 +0300

    rename lastname to firstname

--- Study/Task3/src/ua/Controller/RegularExpressions.java
+++ Study/Task3/src/ua/Controller/RegularExpressions.java
@@ -2,7 +2,7 @@ package ua.Controller;
 
 public interface RegularExpressions {
 
-    String REGULAR_EXPRESSION_LAST_NAME = "[A-Z][a-z]{1,20}";
-    String REGULAR_EXPRESSION_LAST_NAME_UA = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
+    String REGULAR_EXPRESSION_FIRST_NAME = "[A-Z][a-z]{1,20}";
+    String REGULAR_EXPRESSION_FIRST_NAME_UA = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
     String REGULAR_EXPRESSION_NICKNAME ="^[a-zA-Z0-9_.]{8,30}$";
 }
