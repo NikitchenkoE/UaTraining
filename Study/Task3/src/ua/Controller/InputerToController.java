bbca3d11c15d7358010f436e5e5e44a63a7add4d
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-07-05 21:12:28 +0300

    implement UtilityController method to inputToUtility. Add little chek to str, that chek locate

--- Study/Task3/src/ua/Controller/InputerToController.java
+++ Study/Task3/src/ua/Controller/InputerToController.java
@@ -4,16 +4,28 @@ import ua.View.View;
 
 import java.util.Scanner;
 
+import static ua.View.ViewConstants.FIRST_NAME_REQUEST;
+import static ua.View.ViewConstants.NICKNAME_REQUEST;
+
 public class InputerToController {
     private Scanner scanner;
     private View view;
 
+
+    String firstname;
+    String nickname;
     public InputerToController(Scanner scanner, View view) {
         this.scanner = scanner;
         this.view = view;
     }
 
-    public void inputToUtility(){
 
+
+    public void inputToUtility(){
+        UtilityController utilityController = new UtilityController(scanner,view);
+        String str = (String.valueOf(View.bundle.getLocale()).equals("ua"))
+                ? RegularExpressions.REGULAR_EXPRESSION_FIRST_NAME_UA : RegularExpressions.REGULAR_EXPRESSION_FIRST_NAME;
+        this.firstname = utilityController.checkAndOutputInfo(FIRST_NAME_REQUEST,str);
+        this.nickname = utilityController.checkAndOutputInfo(NICKNAME_REQUEST,RegularExpressions.REGULAR_EXPRESSION_NICKNAME);
     }
 }
