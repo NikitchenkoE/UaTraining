39699edf5580ea9b681cf200980d8c47ae09c511
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-07-05 20:45:00 +0300

    little renaming login to nickname

--- Study/Task3/src/ua/View/ViewConstants.java
+++ Study/Task3/src/ua/View/ViewConstants.java
@@ -3,6 +3,6 @@ package ua.View;
 public interface ViewConstants {
       String INPUT_MESSAGE = "input.string.data";
       String FIRST_NAME_REQUEST = "input.first.name.data";
-      String NICKNAME_REQUEST = "input.login.data";
+      String NICKNAME_REQUEST = "input.NICKNAME.data";
       String MISTAKE = "input.mistake.data";
 }
