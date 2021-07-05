058b9b662a548104c13ef784f7ff5fdb7213620d
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-07-05 19:54:55 +0300

    added regular expression for ukr name

--- Study/Task3/src/ua/Controller/RegularExpressions.java
+++ Study/Task3/src/ua/Controller/RegularExpressions.java
@@ -1,6 +1,8 @@
 package ua.Controller;
 
 public interface RegularExpressions {
+
     String REGULAR_EXPRESSION_LAST_NAME = "[A-Z][a-z]{1,20}";
+    String REGULAR_EXPRESSION_LAST_NAME_UA = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
     String REGULAR_EXPRESSION_NICKNAME ="^[a-zA-Z0-9_.]{8,30}$";
 }
