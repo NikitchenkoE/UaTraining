55ea98a4a3cacbe6a7969f50f83e77d811ac2de1
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-07-08 21:43:52 +0300

    added method to set model. But i commented my previous code, because on my strong opinion  exception is superfluous here and make code unreasonably harder

--- Study/Task3/src/ua/Controller/Controller.java
+++ Study/Task3/src/ua/Controller/Controller.java
@@ -1,11 +1,16 @@
 package ua.Controller;
 
+import ua.Model.DataBaseImitation;
 import ua.Model.Model;
+import ua.Model.UsedNicknameException;
 import ua.View.View;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static ua.View.ViewConstants.USED_NICKNAME;
 
 public class Controller {
     Model model;
@@ -20,15 +25,29 @@ public class Controller {
         Scanner scan = new Scanner(System.in);
         InputerToController inputer = new InputerToController(scan, view);
         inputer.inputToUtility();
-        setToModel(inputer.getFirstname(), inputer.getNickname());
+        Model model = setModel(inputer);
         model.formReader();
     }
 
-    public void setToModel(String firstname, String nickname){
-        model.setNickname(nickname);
-        model.setFirstname(firstname);
+    private Model setModel(InputerToController inputer) {
+//        while (DataBaseImitation.checkNickname(inputer.getNickname())==true){
+//            System.out.println("Nickname '" + inputer.getNickname()+ "' already exist");
+//            inputer.inputNickname();
+//        }
+//        Model model = new Model(inputer.getFirstname(),inputer.getNickname());
+//        return model;
+        Model model = null;
+        while (true) {
+            try {
+                model = new Model(inputer.getFirstname(), inputer.getNickname());
+                break;
+            } catch (UsedNicknameException exception) {
+                System.out.println("Nickname '" + inputer.getNickname() + "' already exist");
+                inputer.inputNickname();
+            }
+        }
+        return model;
     }
-
 
 }
 
